6857b592fdb994ebfeda41c4e7f956a7b4564829
Antea P <antea.primorac0@gmail.com>
2024-07-29 16:57:04 +0200

    isolated URL into Constants

--- app/src/main/java/rs/ac/metropolitan/cs330_pz_anteaprimorac5157/data/network/RetrofitClient.kt
+++ app/src/main/java/rs/ac/metropolitan/cs330_pz_anteaprimorac5157/data/network/RetrofitClient.kt
@@ -2,9 +2,9 @@ package rs.ac.metropolitan.cs330_pz_anteaprimorac5157.data.network
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import rs.ac.metropolitan.cs330_pz_anteaprimorac5157.data.network.Constants.BASE_URL
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.89.15:3001/api/v1/"
 
     val instance: DreamDiaryApi by lazy {
         Retrofit.Builder()
