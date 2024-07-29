564a4183e257f07441c22175923003ba24c70188
Antea P <antea.primorac0@gmail.com>
2024-07-29 14:45:50 +0200

    initial commit: default files + added network_security_config + updated AndroidManifest,  libs.versions.toml and build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,125 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "rs.ac.metropolitan.cs330_pz_anteaprimorac5157"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "rs.ac.metropolitan.cs330_pz_anteaprimorac5157"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        // testInstrumentationRunner = "rs.ac.metropolitan.cs330_pz_anteaprimorac5157.HiltTestRunner"
+        // testInstrumentationRunnerArguments["androidx.work.multiprocess"] = "true"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.jvmTarget.get())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.jvmTarget.get())
+    }
+    kotlinOptions {
+        jvmTarget = libs.versions.jvmTarget.get()
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.kotlinCompiler.get()
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
+
+    //View Model
+    implementation(libs.viewModel.compose)
+    implementation(libs.viewModel.ktx)
+    implementation(libs.navigation.compose)
+
+    //LiveData
+    implementation(libs.livedata.ktx)
+    implementation(libs.livedata.runtime)
+    implementation(libs.compose.livedata)
+
+    //Coil
+    implementation(libs.coil)
+    implementation(libs.coil.compose)
+
+    //Room
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.junit.ktx)
+    implementation(libs.play.services.location)
+    implementation(libs.kotlinx.coroutines.play.services)
+    implementation(libs.androidx.appcompat)
+    ksp(libs.room.compiler)
+
+    //Retrofit
+    implementation(libs.gson)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
+    //implementation(libs.retrofit.coroutines)
+
+    //Hilt
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
+    implementation(libs.hilt.navigation.compose)
+
+    //Testing
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+    testImplementation(libs.coroutines.test)
+    testImplementation(libs.arch.core.testing)
+    testImplementation(libs.mockwebserver)
+    androidTestImplementation(libs.junit.ext)
+    androidTestImplementation(libs.arch.core.testing)
+    androidTestImplementation(libs.coroutines.test)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+    androidTestImplementation(libs.mockk)
+    androidTestImplementation(libs.mockwebserver)
+    kspAndroidTest(libs.hilt.compiler)
+    androidTestImplementation(libs.hilt.testing)
+
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+
+}
